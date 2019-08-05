830074db11a50f610775f378da6aa08a74e3aa0e
Christopher Kosik <kosik.chris.j@gmail.com>
2019-08-05 16:21:44 -0400

    UI updates + bargraph

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, Component } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,21 +7,28 @@ import {
   Text,
   TouchableOpacity,
   PermissionsAndroid,
-  Image
-} from "react-native";
+  Image,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
 
-import Modal from "react-native-modal";
-import RNRestart from "react-native-restart";
-import { LineChart } from "react-native-svg-charts";
+import { Footer, FooterTab} from 'native-base';
 
-import { BleManager, ScanMode } from "react-native-ble-plx";
-import { BluetoothStatus } from "react-native-bluetooth-status";
-import { getDecFrom64 } from "./assets/utility/DecFrom64";
+import Modal from 'react-native-modal';
+import RNRestart from 'react-native-restart';
+import { LineChart } from 'react-native-svg-charts';
 
-let DegreeComponent = require("./components/DegreeComponent");
+
+import { VictoryBar, VictoryTheme, VictoryChart, VictoryLine } from 'victory-native';
+
+import { BleManager, ScanMode } from 'react-native-ble-plx';
+import { BluetoothStatus } from 'react-native-bluetooth-status';
+import { getDecFrom64 } from './assets/utility/DecFrom64';
+
+let DegreeComponent = require('./components/DegreeComponent');
 
 const adcCelciusScalar = 0.0078125;
-const tiServiceID = "0000fff0-0000-1000-8000-00805f9b34fb";
+const tiServiceID = '0000fff0-0000-1000-8000-00805f9b34fb';
 let ScanOptions = { scanMode: ScanMode.LowLatency };
 
 //Create graph component to be added into Main Component
@@ -31,12 +38,13 @@ export default class Main extends Component {
     super();
     this.state = {
       permissionState: false,
-      bluetoothState: "",
+      bluetoothState: '',
       adcValue: null,
       decimalVal: null,
       celciusVal: null,
       farenheitVal: null,
-      device: null
+      device: null,
+      orientation: ''
     };
     this.manager = new BleManager();
     this.requestPermissions();
@@ -47,13 +55,13 @@ export default class Main extends Component {
       const coarseGrant = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
         {
-          title: "Bluetooth Low Energy Permissions",
+          title: 'Bluetooth Low Energy Permissions',
           message:
-            "WMUTemperatureBeacon needs access to your" +
-            "coarse location in order to use Bluetooth Low Energy. " +
-            "This app will not work without this permission.",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
+            'WMUTemperatureBeacon needs access to your' +
+            'coarse location in order to use Bluetooth Low Energy. ' +
+            'This app will not work without this permission.',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK'
           // eslint-disable-next-line no-undef
         }
       );
@@ -71,9 +79,9 @@ export default class Main extends Component {
 
       if (coarseGrant === PermissionsAndroid.RESULTS.GRANTED) {
         this.state.permissionState = true;
-        console.log("BLE coarse location permission granted.");
+        console.log('BLE coarse location permission granted.');
       } else {
-        console.log("BLE coarse location permissions denied.");
+        console.log('BLE coarse location permissions denied.');
       }
     } catch (error) {
       console.warn(error);
@@ -83,15 +91,20 @@ export default class Main extends Component {
   componentDidMount() {
     //console.log("mounted!");
     //console.log(this.state.permissionState);
+    this.getOrientation();
     this.manager.enable();
-    console.log("manager enabled");
+    console.log('manager enabled');
+    Dimensions.addEventListener( 'change', () =>
+    {
+      this.getOrientation();
+    });
     //requestPermissions();
     const subscription = this.manager.onStateChange(state => {
-      if (state === "PoweredOn") {
+      if (state === 'PoweredOn') {
         this.scanObserverValues();
         //subscription.remove();
       }
-      if (state === "PoweredOff") {
+      if (state === 'PoweredOff') {
       }
       //this.scanObserverValues();
     }, true);
@@ -99,16 +112,31 @@ export default class Main extends Component {
 
   async checkInitialBluetoothState() {
     const isEnabled = await BluetoothStatus.state();
-    this.setState({ bluetoothState: isEnabled ? "On" : "Off" });
+    this.setState({ bluetoothState: isEnabled ? 'On' : 'Off' });
   }
 
   async toggleBluetooth() {
     try {
       const isEnabled = await BluetoothStatus.state();
       BluetoothStatus.enable(!isEnabled);
-      this.setState({ bluetoothState: isEnabled ? "Off" : "On" });
+      this.setState({ bluetoothState: isEnabled ? 'Off' : 'On' });
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  getOrientation = () =>
+  {
+    if( this.refs.rootView )
+    {
+        if( Dimensions.get('window').width < Dimensions.get('window').height )
+        {
+          this.setState({ orientation: 'portrait' });
+        }
+        else
+        {
+          this.setState({ orientation: 'landscape' });
+        }
     }
   }
 
@@ -183,8 +211,26 @@ export default class Main extends Component {
       adcValue,
       celciusVal,
       farenheitVal,
-      permissionState
+      permissionState,
     } = this.state;
+
+     let chartHeight;
+
+     if(this.state.orientation === 'portrait') {
+      chartHeight = Dimensions.get('window').height/2;
+     }
+     if(this.state.orientation === 'landscape') {
+      chartHeight = 300;
+     }
+
+
+    // if (Dimensions.get("window").width < Dimensions.get("window").height) {
+    //   chartHeight = Dimensions.get('window').height/2;
+    // }
+    // if(Dimensions.get('window').width > Dimensions.get('window').height) {
+    //   chartHeight = 300;
+    // }
+
     if (adcValue != null) {
       return (
         <Fragment>
@@ -194,18 +240,18 @@ export default class Main extends Component {
           >
             <View style={styles.logos}>
               <Image
-                source={require("./assets/resources/images/Western_Michigan_University_wordmark.svg__300x100.png")}
+                source={require('./assets/resources/images/Western_Michigan_University_wordmark.svg__300x100.png')}
                 style={styles.image}
               />
               <Image
-                source={require("./assets/resources/images/SafeSense_Technologies_Logo_300x100.jpg")}
+                source={require('./assets/resources/images/SafeSense_Technologies_Logo_300x100.jpg')}
                 style={styles.image}
               />
             </View>
             <View style={styles.body}>
               <View style={styles.headerRow}>
                 <View style={styles.rowItemBold}>
-                  <Text style={{ fontWeight: "700" }}>Device Name</Text>
+                  <Text style={{ fontWeight: '700' }}>Device Name</Text>
                 </View>
                 <View style={styles.rowItemBold}>
                   <DegreeComponent
@@ -216,7 +262,7 @@ export default class Main extends Component {
                       marginTop: 0
                     }}
                   />
-                  <Text style={{ fontWeight: "700" }}>C</Text>
+                  <Text style={{ fontWeight: '700' }}>C</Text>
                 </View>
                 <View style={styles.rowItemBold}>
                   <DegreeComponent
@@ -227,7 +273,7 @@ export default class Main extends Component {
                       marginTop: 0
                     }}
                   />
-                  <Text style={{ fontWeight: "700" }}>F</Text>
+                  <Text style={{ fontWeight: '700' }}>F</Text>
                 </View>
               </View>
               <View style={styles.row}>
@@ -259,8 +305,44 @@ export default class Main extends Component {
                   <Text>F</Text>
                 </View>
               </View>
+              <View style={styles.chartContainer}>
+              <VictoryChart
+                height={chartHeight}
+                width={Dimensions.get('window').width*0.9}
+                domain={{ y: [10.00, 40.00] }}
+                theme={VictoryTheme.material}>
+                <VictoryBar
+                  barRatio={1.0}
+                  barWidth={150}
+                    style={{ data: { fill: '#4FC1E9' } }}
+                  alignment="middle"
+                  data={[
+                    {
+                      x: 'Celcius',
+                      y: Number(celciusVal)
+                      }
+                  ]}
+                    labels={d => `${d.y}°C`}
+                  />
+                </VictoryChart>
+              </View>
               {/* <View>
-              <Text>YOLO</Text>
+                <VictoryChart
+                  domain={{ y: [15.0, 40.0] }}
+                  theme={VictoryTheme.material}>
+                  <VictoryLine
+                    style={{ data: { fill: "#4FC1E9" } }}
+                    data={[
+                      {
+                        x: 5,
+                        y: 2
+                      }
+                     ]}
+                    //labels={(datum) => `${datum.y}°C`}
+                  />
+                </VictoryChart>
+              </View> */}
+              {/* <View>
                 <LineChart
                   style={{
                     flex: 1,
@@ -278,6 +360,11 @@ export default class Main extends Component {
                 />
               </View> */}
             </View>
+            <Footer>
+              <FooterTab style={styles.footer}>
+                <Text>Copyright ©2019 SafeSense LLC.</Text>
+              </FooterTab>
+            </Footer>
           </ScrollView>
         </Fragment>
       );
@@ -292,15 +379,23 @@ export default class Main extends Component {
         >
           <View style={styles.logos}>
             <Image
-              source={require("./assets/resources/images/Western_Michigan_University_wordmark.svg__300x100.png")}
+              source={require('./assets/resources/images/Western_Michigan_University_wordmark.svg__300x100.png')}
               style={styles.image}
             />
             <Image
-              source={require("./assets/resources/images/SafeSense_Technologies_Logo_300x100.jpg")}
+              source={require('./assets/resources/images/SafeSense_Technologies_Logo_300x100.jpg')}
               style={styles.image}
             />
           </View>
-          <Text>Searching for devices...</Text>
+          <View style={styles.body}>
+            <Text style={{alignSelf: 'center'}}>Searching for devices...</Text>
+            <ActivityIndicator size="large" color="#0000ff" style={{alignSelf: "center", marginTop: 50}}/>
+          </View>
+          <Footer>
+            <FooterTab style={styles.footer}>
+              <Text>Copyright ©2019 SafeSense LLC.</Text>
+            </FooterTab>
+          </Footer>
         </ScrollView>
       </Fragment>
     );
@@ -309,7 +404,7 @@ export default class Main extends Component {
 
 const styles = StyleSheet.create({
   scrollView: {
-    backgroundColor: "#FFFFFF"
+    backgroundColor: '#FFFFFF'
   },
   // engine: {
   //   position: 'absolute',
@@ -317,25 +412,60 @@ const styles = StyleSheet.create({
   // },
   body: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    alignItems: "flex-start",
-    flexDirection: "column"
+    backgroundColor: '#FFFFFF',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    height: '90%'
   },
   logos: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    alignItems: "flex-start",
-    flexDirection: "row",
+    backgroundColor: '#FFFFFF',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
     paddingTop: 10,
     paddingBottom: 30,
     borderBottomWidth: 1,
-    borderBottomColor: "#000000"
+    borderBottomColor: '#000000'
   },
   image: {
     flex: 1,
     width: 50,
     height: 50,
-    resizeMode: "contain"
+    resizeMode: 'contain'
+  },
+  item: {
+    flexDirection: 'column',
+    marginBottom: 5,
+    paddingHorizontal: 10
+  },
+  label: {
+    color: '#CBCBCB',
+    flex: 1,
+    fontSize: 12,
+    position: 'relative',
+    top: 2
+  },
+  bar: {
+    alignSelf: 'center',
+    borderRadius: 5,
+    height: 8,
+    marginRight: 5
+  },
+  celcius: {
+    backgroundColor: '#F55443'
+  },
+  chartContainer : {
+    flexDirection: 'column',
+    marginTop: 6,
+    alignItems: 'center'
+  },
+  data: {
+    flex: 2,
+    flexDirection: 'row'
+  },
+  dataNumber: {
+    color: '#CBCBCB',
+    fontSize: 20
   },
   // sectionContainer: {
   //   marginTop: 32,
@@ -353,7 +483,7 @@ const styles = StyleSheet.create({
   //   color: Colors.dark,
   // },
   highlight: {
-    fontWeight: "700"
+    fontWeight: '700'
   },
   // footer: {
   //   color: Colors.dark,
@@ -364,34 +494,50 @@ const styles = StyleSheet.create({
   //   textAlign: 'right',
   // },
   headerRow: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginVertical: 10,
     paddingBottom: 10,
     paddingRight: 15,
     paddingLeft: 15,
     marginBottom: 5,
-    justifyContent: "space-between",
-    alignItems: "center"
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontSize: 10
   },
   row: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginVertical: 10,
     paddingBottom: 10,
     paddingRight: 15,
     paddingLeft: 15,
     marginBottom: 5,
-    justifyContent: "space-between",
-    alignItems: "center"
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    fontSize: 100
   },
   rowItem: {
     padding: 1,
-    width: "33%",
-    flexDirection: "row"
+    width: '33%',
+    flexDirection: 'row'
   },
   rowItemBold: {
     padding: 1,
-    width: "33%",
-    flexDirection: "row"
+    width: '33%',
+    flexDirection: 'row'
+  },
+  footerTab : {
+    height: 50,
+    width: '100%',
+    position: 'absolute',
+    bottom: 0
+  },
+  footer : {
+    backgroundColor: '#ACF7F7',
+    height: 50,
+    width: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 0
   }
 });
 
